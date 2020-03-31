7946456806e2ed353b01b1d1e99883a263706c3d
Nandita Venkat <nanditavenkat@gmail.com>
2020-03-31 19:39:42 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
 current_time = Time.now
-current_time=current_time.to_
-if current_time%2==0
+current_time = current_time.to_i
+ 
+if current_time.even?
   puts "Even!"
-  
-  else
+else
   puts "Odd!"
 end
